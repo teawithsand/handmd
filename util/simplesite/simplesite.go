922009ffb1673bffdc575d05d9a9eb6633f99f69
teawithsand <teawithsand@gmail.com>
2022-04-07 15:36:41 +0000

    Added poor man's port of simple HTML renderer

---
+++ util/simplesite/simplesite.go
@@ -0,0 +1,36 @@
+package simplesite
+
+import "html/template"
+
+const rawTemplate = `<!DOCTYPE html>
+<html lang="{{ .Lang }}">
+<head>
+	<meta charset="utf-8">
+	<title>{{ .Title }}</title>
+	{{ range .Inputs }}
+		{{ .Render}}
+	{{ end }}
+	{{range .Metas }}
+		{{ .Render}}
+	{{end}}
+	{{range .Links}}
+		{{ .Render }}
+	{{end}}
+	{{range .Scripts}}
+		{{ .Render }}
+	{{end}}
+</head>
+<body>
+</body>
+</html>`
+
+func getCompiledTemplate() *template.Template {
+	tpl, err := template.New("asdf").Parse(rawTemplate)
+	if err != nil {
+		panic(err)
+	}
+
+	return tpl
+}
+
+var compiledTemplate = getCompiledTemplate()
