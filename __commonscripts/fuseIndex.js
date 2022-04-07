7e21f315137a6de285d06e0bcc64e6b88896cdf4
teawithsand <teawithsand@gmail.com>
2022-04-07 15:19:46 +0000

    Added util script for creating fuse.js indexes

---
+++ __commonscripts/fuseIndex.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+
+/**
+ * Simple node script, which creates fuse.js index from data comming from stdin.
+ */
+
+const fuse = require("fuse.js")
+const fs = require("fs")
+
+
+const data = fs.readFileSync(0, 'utf-8');
+const parsed = JSON.parse(data)
+
+const fields = process.argv.slice(2)
+const index = fuse.createIndex(fields, parsed)
+
+process.stdout.write(JSON.stringify(index))
