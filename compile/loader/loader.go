91a7a3cd8a6fc43391accc85bc96230b59348b5f
teawithsand <teawithsand@gmail.com>
2022-04-10 09:48:44 +0000

    Fixed LoaderFunc

--- compile/loader/loader.go
+++ compile/loader/loader.go
@@ -14,6 +14,10 @@ type Loader[C any, O any] interface {
 
 type LoaderFunc[C any, O any] func(ctx context.Context, lctx C, input LoaderInput) (output O, err error)
 
-func (f LoaderFunc[C, O]) Render(ctx context.Context, lctx C, input LoaderInput) (output O, err error) {
+func (f LoaderFunc[C, O]) Load(ctx context.Context, lctx C, input LoaderInput) (output O, err error) {
 	return f(ctx, lctx, input)
 }
+
+var _ Loader[any, any] = LoaderFunc[any, any](func(ctx context.Context, lctx any, input LoaderInput) (output any, err error) {
+	return
+})
