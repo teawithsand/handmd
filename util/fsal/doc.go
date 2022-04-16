085ff4fbcb24d6b086e099d5a9914df788a1cf0a
teawithsand <teawithsand@gmail.com>
2022-04-16 06:50:23 +0000

    Fixed typo

--- util/fsal/doc.go
+++ util/fsal/doc.go
@@ -2,5 +2,5 @@
 // It abstract basic file system operations.
 //
 // It's not general-purpose FS abstraction. It's too simple for it. For instance, it does not have permission.
-// But it's fully sufficent for handmd purposes.
+// But it's fully sufficient for handmd purposes.
 package fsal
