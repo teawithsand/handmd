6501601625f68a7f92ec5803f395a90feaf0b678
teawithsand <teawithsand@gmail.com>
2022-04-16 06:25:35 +0000

    Refactored command and added ExecBackground option

--- util/scripting/command.go
+++ util/scripting/command.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"os/exec"
+	"sync/atomic"
 )
 
 // Wrapper for go exec.Command type, which is simpler.
@@ -14,35 +15,54 @@ type Command struct {
 	Args    []string
 	EnvVars map[string]string
 
+	// If empty, current process' CWD is used
+	Dir string
+
 	Stdin  io.Reader
 	Stdout io.Writer
 	Stderr io.Writer
 }
 
 func (c *Command) Exec(ctx context.Context) (err error) {
-	cmd := exec.Command(c.Command, c.Args...)
+	cmd := exec.CommandContext(ctx, c.Command, c.Args...)
 	cmd.Stdout = c.Stdout
 	cmd.Stdin = c.Stdin
 	cmd.Stderr = c.Stderr
+	cmd.Dir = c.Dir
 
 	for k, v := range c.EnvVars {
 		cmd.Env = append(cmd.Env, fmt.Sprintf("%s=%s", k, v))
 	}
 
-	doneChan := make(chan error)
+	err = cmd.Run()
+	return
+}
+
+// Executes command until:
+// 1. Context gets cancelled
+// 2. close function gets called.
+//
+// Once command gets closed, result error is returned through result channel.
+// If command was closed via returned closer, nil error is emitted.
+// Panics, if channel is closed.
+//
+// Not closing command causes goroutine leak, so it should be always closed either
+// via canceling context or calling closer function.
+func (c *Command) ExecBackground(ctx context.Context, res chan<- error) (close func()) {
+	var flag int32 // 1 once result was sent via channel
+
+	ctx, canceler := context.WithCancel(ctx)
 	go func() {
-		doneChan <- cmd.Run()
+		err := c.Exec(ctx)
+		if atomic.CompareAndSwapInt32(&flag, 0, 1) {
+			res <- err
+		}
 	}()
 
-	select {
-	case <-ctx.Done():
-		cmd.Process.Kill()
-
-		err = ctx.Err()
-		return
-	case err = <-doneChan:
-		if err != nil {
-			return
+	close = func() {
+		if atomic.CompareAndSwapInt32(&flag, 0, 1) {
+			res <- nil
+			canceler()
 		}
 	}
 
